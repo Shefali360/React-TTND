356cefe27d3cee18c9e37526ef8523e9226d02ce
Shefali360 <shefali.goyal@tothenew.com>
2020-07-04 22:09:43 +0530

    Created custom component for delete popup

---
+++ src/components/DeleteAssurance/DeleteAssurance.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './DeleteAssurance.module.css';
+
+const DeleteAssurance=(props)=>{
+    return(
+        <div className={styles.overlay}>
+            <div className={styles.popup}>
+            <h5>
+             Delete {props.name}
+          </h5>
+          <p>{props.message}</p>
+          <button
+            className={styles.delete}
+              type="button"
+              value="submit"
+            //   disabled={this.state.submitDisabled}
+              onClick={props.delete}
+            >
+              Delete
+            </button>
+            <button
+              type="button"
+              className={styles.cancel}
+              onClick={
+                props.cancel
+              }
+            >
+              Cancel
+            </button>
+            </div>
+          </div>
+    );
+}
+
+export default DeleteAssurance;
