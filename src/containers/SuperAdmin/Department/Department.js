44e2cf885cb86248cf71264c8697094628d82439
Shefali360 <shefali.goyal@tothenew.com>
2020-07-05 15:01:00 +0530

    Formatted file

--- src/containers/SuperAdmin/Department/Department.js
+++ src/containers/SuperAdmin/Department/Department.js
@@ -2,83 +2,83 @@ import React, { Component } from "react";
 import complaintStyles from "../../../containers/ComplaintPage/ComplaintList/ComplaintList.module.css";
 import Spinner from "../../../components/Spinner/Spinner";
 import styles from "./Department.module.css";
-import {authorizedRequestsHandler} from "../../../APIs/APIs";
-import {departmentEndpoint} from "../../../APIs/APIEndpoints";
+import { authorizedRequestsHandler } from "../../../APIs/APIs";
+import { departmentEndpoint } from "../../../APIs/APIEndpoints";
 import DeleteAssurancePopup from "../../../components/DeleteAssurance/DeleteAssurance";
 import DepartmentPopup from "./DepartmentPopup/DepartmentPopup";
-import {stringify} from "querystring";
+import { stringify } from "querystring";
 
 class Department extends Component {
+  state = {
+    id: "",
+    deptid: "",
+    dept: "",
+    departmentList: [],
+    popupVisible: false,
+    deletePopupVisible: false,
+    department: "",
+    submitDisabled: false,
+    editPopupVisible: false,
+  };
 
-  state={
-    id:'',
-    deptid:'',
-    dept:'',
-    departmentList:[],
-    popupVisible:false,
-    deletePopupVisible:false,
-    department:'',
-    submitDisabled:false,
-    editPopupVisible:false
-  }
-
-  openPopup=()=>{
+  openPopup = () => {
     this.setState({
-      popupVisible:true
-    })
-  }
+      popupVisible: true,
+    });
+  };
 
-  closeEditPopup=()=>{
-    this.setState({editPopupVisible:false})
-  }
+  closeEditPopup = () => {
+    this.setState({ editPopupVisible: false });
+  };
 
-  closePopup=()=>{
-    this.setState({popupVisible:false});
-  }
+  closePopup = () => {
+    this.setState({ popupVisible: false });
+  };
 
-  handleChange=(event)=>{
-    this.setState({
-     [event.target.name]:event.target.value
-    },
-    // () => {
-    //     if (event.target.name !=="")
-    //     { this.setState({ submitDisabled:false });}
-    // }
-    )
-  }
+  handleChange = (event) => {
+    this.setState(
+      {
+        [event.target.name]: event.target.value,
+      }
+      // () => {
+      //     if (event.target.name !=="")
+      //     { this.setState({ submitDisabled:false });}
+      // }
+    );
+  };
 
   submitHandler = (event) => {
     event.preventDefault();
     const department = {
-      department:this.state.department
+      department: this.state.department,
     };
-   authorizedRequestsHandler()
+    authorizedRequestsHandler()
       .post(departmentEndpoint, department)
       .then((res) => {
         this.props.deptArray.push(res.data);
         this.setState({
-          popupVisible:false,
-          submitDisabled: true
+          popupVisible: false,
+          submitDisabled: true,
         });
       })
       .catch((err) => {
-        const errorCode=err.response.data.errorCode;
-        if(errorCode==="INVALID_TOKEN"){
-           this.props.errorOccurred();
+        const errorCode = err.response.data.errorCode;
+        if (errorCode === "INVALID_TOKEN") {
+          this.props.errorOccurred();
         }
-        if(err.response.status===500){
-          this.setState({networkErr:true});
+        if (err.response.status === 500) {
+          this.setState({ networkErr: true });
         }
       });
   };
 
-  showDeletePopup=(id)=>{
-    this.setState({deletePopupVisible:true,id:id});
-  }
+  showDeletePopup = (id) => {
+    this.setState({ deletePopupVisible: true, id: id });
+  };
 
-  closeDeletePopup=()=>{
-    this.setState({deletePopupVisible:false});
-  }
+  closeDeletePopup = () => {
+    this.setState({ deletePopupVisible: false });
+  };
 
   submitEditHandler = (event) => {
     event.preventDefault();
@@ -87,15 +87,15 @@ class Department extends Component {
     };
     // this.setState({ spinner: true});
     authorizedRequestsHandler()
-      .patch(departmentEndpoint+`/${this.state.deptid}`,formData)
+      .patch(departmentEndpoint + `/${this.state.deptid}`, formData)
       .then((res) => {
-        const array=this.props.deptArray;
-        const index=array.findIndex(ele=>ele._id===this.state.deptid);
-        array[index].department=res.data.department;
+        const array = this.props.deptArray;
+        const index = array.findIndex((ele) => ele._id === this.state.deptid);
+        array[index].department = res.data.department;
         this.setState({
-          departmentList:array,
+          departmentList: array,
           dept: "",
-          editPopupVisible:false
+          editPopupVisible: false,
           // formSubmitted: true,
           // submitDisabled: true,
           // spinner: false,
@@ -118,19 +118,22 @@ class Department extends Component {
       });
   };
 
-
-  editDepartment=(id)=>{
-   const filter={};
-   if(id){
-     filter["_id"]=id;
-   }
-   authorizedRequestsHandler()
-   .get(departmentEndpoint+"?"+stringify(filter))
-   .then((res)=>{
-     this.setState({deptid:res.data[0]._id,dept:res.data[0].department,editPopupVisible:true});
-   })
-   .catch((err)=>console.log(err));
-  }
+  editDepartment = (id) => {
+    const filter = {};
+    if (id) {
+      filter["_id"] = id;
+    }
+    authorizedRequestsHandler()
+      .get(departmentEndpoint + "?" + stringify(filter))
+      .then((res) => {
+        this.setState({
+          deptid: res.data[0]._id,
+          dept: res.data[0].department,
+          editPopupVisible: true,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
 
   deleteDepartment = () => {
     authorizedRequestsHandler()
@@ -143,7 +146,7 @@ class Department extends Component {
             break;
           }
         }
-        this.setState({deletePopupVisible:false,departmentList:arr});
+        this.setState({ deletePopupVisible: false, departmentList: arr });
       })
       .catch((err) => {
         const errorCode = err.response.data.errorCode;
@@ -155,7 +158,6 @@ class Department extends Component {
         }
       });
   };
-
 
   render() {
     let dept = null;
@@ -171,13 +173,13 @@ class Department extends Component {
       dept = (
         <tr>
           <td>
-            <i className="fa fa-exclamation-triangle">
-            </i>Department List can't be loaded.
+            <i className="fa fa-exclamation-triangle"></i>Department List can't
+            be loaded.
           </td>
         </tr>
       );
     } else if (this.props.deptArray.length === 0)
-     dept = (
+      dept = (
         <tr>
           <td>Table has no data.</td>
         </tr>
@@ -188,9 +190,17 @@ class Department extends Component {
         return (
           <tr key={dept._id}>
             <td>
-            {dept.department}
-            <i className={["fa fa-edit",styles.edit].join(' ')} onClick={()=>this.editDepartment(dept._id)} title="Edit Department"></i>
-            <i className={["fa fa-trash",styles.delete].join(' ')} onClick={()=>this.showDeletePopup(dept._id)} title="Delete Department"></i>
+              {dept.department}
+              <i
+                className={["fa fa-edit", styles.edit].join(" ")}
+                onClick={() => this.editDepartment(dept._id)}
+                title="Edit Department"
+              ></i>
+              <i
+                className={["fa fa-trash", styles.delete].join(" ")}
+                onClick={() => this.showDeletePopup(dept._id)}
+                title="Delete Department"
+              ></i>
             </td>
           </tr>
         );
@@ -198,12 +208,16 @@ class Department extends Component {
     }
 
     return (
-      <div className={[complaintStyles.complaintsList,styles.box].join(' ')}>
+      <div className={[complaintStyles.complaintsList, styles.box].join(" ")}>
         {this.props.state.networkErr
           ? alert("Please check your internet connection")
           : null}
         <h4>All Departments</h4>
-        <i className={["fa fa-building-o",styles.icon].join(' ')} onClick={this.openPopup} title="Add Department"></i>
+        <i
+          className={["fa fa-building-o", styles.icon].join(" ")}
+          onClick={this.openPopup}
+          title="Add Department"
+        ></i>
         <div className={complaintStyles.tableContainer}>
           <table>
             <thead>
@@ -211,24 +225,65 @@ class Department extends Component {
                 <th>Department</th>
               </tr>
             </thead>
-            <tbody>
-            {dept||[]}
-            </tbody>
+            <tbody>{dept || []}</tbody>
           </table>
-          {(this.state.deletePopupVisible)?<DeleteAssurancePopup name="Department" message="Want to proceed with the deletion?" delete={this.deleteDepartment} cancel={this.closeDeletePopup}/>:null}
-          <div className={styles.overlay+" "+(this.state.popupVisible?"null":styles.display)}>
-          <div className={styles.popup +" "+(this.state.popupVisible?"null":styles.display)}>
-            <DepartmentPopup heading="Add New Department" click={this.closePopup}
-            name="department" value={this.state.department} change={this.handleChange}
-            disabled={this.state.submitDisabled} submit={(event)=>this.submitHandler(event)}/>
+          {this.state.deletePopupVisible ? (
+            <DeleteAssurancePopup
+              name="Department"
+              message="Want to proceed with the deletion?"
+              delete={this.deleteDepartment}
+              cancel={this.closeDeletePopup}
+            />
+          ) : null}
+          <div
+            className={
+              styles.overlay +
+              " " +
+              (this.state.popupVisible ? "null" : styles.display)
+            }
+          >
+            <div
+              className={
+                styles.popup +
+                " " +
+                (this.state.popupVisible ? "null" : styles.display)
+              }
+            >
+              <DepartmentPopup
+                heading="Add New Department"
+                click={this.closePopup}
+                name="department"
+                value={this.state.department}
+                change={this.handleChange}
+                disabled={this.state.submitDisabled}
+                submit={(event) => this.submitHandler(event)}
+              />
+            </div>
           </div>
-          </div>
-          <div className={styles.overlay+" "+(this.state.editPopupVisible?"null":styles.display)}>
-          <div className={styles.popup +" "+(this.state.editPopupVisible?"null":styles.display)}>
-            <DepartmentPopup heading="Edit Department" click={this.closeEditPopup}
-            name="dept" value={this.state.dept} change={this.handleChange}
-            disabled={this.state.submitDisabled} submit={(event)=>this.submitEditHandler(event)}/>
-          </div>
+          <div
+            className={
+              styles.overlay +
+              " " +
+              (this.state.editPopupVisible ? "null" : styles.display)
+            }
+          >
+            <div
+              className={
+                styles.popup +
+                " " +
+                (this.state.editPopupVisible ? "null" : styles.display)
+              }
+            >
+              <DepartmentPopup
+                heading="Edit Department"
+                click={this.closeEditPopup}
+                name="dept"
+                value={this.state.dept}
+                change={this.handleChange}
+                disabled={this.state.submitDisabled}
+                submit={(event) => this.submitEditHandler(event)}
+              />
+            </div>
           </div>
         </div>
       </div>
