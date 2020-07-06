edca816b61d4d206eeb00e74a211ead9cd3c74c3
Shefali360 <shefali.goyal@tothenew.com>
2020-07-06 23:21:24 +0530

    indentation of API file

--- src/APIs/APIs.js
+++ src/APIs/APIs.js
@@ -1,36 +1,36 @@
-import axios from 'axios';
-import { serverURI } from './APIEndpoints';
-import { store } from '../store/store';
+import axios from "axios";
+import { serverURI } from "./APIEndpoints";
+import { store } from "../store/store";
 
 let headers = {};
-let authenticatedRequests=null;
-let authorizedRequests=null;
+let authenticatedRequests = null;
+let authorizedRequests = null;
 
 const listener = () => {
   let token = store.getState().auth.token;
   headers = {
-      authorization: `Bearer ${token.access_token},Bearer ${token.id_token}`
-  }
+    authorization: `Bearer ${token.access_token},Bearer ${token.id_token}`,
+  };
 };
 
 export const authenticatedRequestsHandler = () => {
-  if(authenticatedRequests){
+  if (authenticatedRequests) {
     return authenticatedRequests;
   }
-  authenticatedRequests=axios.create({
-    baseURL: serverURI 
+  authenticatedRequests = axios.create({
+    baseURL: serverURI,
   });
   return authenticatedRequests;
-}
+};
 
 export const authorizedRequestsHandler = () => {
   listener();
-  if(authorizedRequests){
+  if (authorizedRequests) {
     return authorizedRequests;
   }
-  authorizedRequests=axios.create({
+  authorizedRequests = axios.create({
     baseURL: serverURI,
-    headers: headers
+    headers: headers,
   });
   return authorizedRequests;
-}
+};
